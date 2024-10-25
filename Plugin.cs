1093e4ce631c5fda2461a60cd760b7d57d0d31e9
ButteryStancakes <jlj3322@gmail.com>
2024-10-25 02:57:26 -0400

    v66 update

--- Plugin.cs
+++ Plugin.cs
@@ -9,13 +9,25 @@ namespace ClassicSuitRestoration
     [BepInDependency("butterystancakes.lethalcompany.keepunlocks", BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
     {
-        const string PLUGIN_GUID = "butterystancakes.lethalcompany.classicsuitrestoration", PLUGIN_NAME = "Classic Suit Restoration", PLUGIN_VERSION = "2.0.3";
-        public static ConfigEntry<bool> configUnlockable;
+        const string PLUGIN_GUID = "butterystancakes.lethalcompany.classicsuitrestoration", PLUGIN_NAME = "Classic Suit Restoration", PLUGIN_VERSION = "2.1.0";
+        public static ConfigEntry<bool> configUnlockable, configOfficial, configBirthday;
 
         internal static new ManualLogSource Logger;
 
         void Awake()
         {
+            configOfficial = Config.Bind(
+                "Suit",
+                "Official",
+                true,
+                "Uses the official brown suit texture from v65.\nNOTE: This texture is official, but it is a recreation in the final game's visual style, and not the actual suit texture from earlier versions of the game.");
+
+            configBirthday = Config.Bind(
+                "Suit",
+                "Birthday",
+                false,
+                "If using the anniversary suit (from the \"Official\" setting), this will complete the costume by adding back the party hat as well.");
+
             configUnlockable = Config.Bind(
                 "Misc",
                 "Unlockable",
--- RestoreClassicSuit.cs
+++ RestoreClassicSuit.cs
@@ -22,7 +22,7 @@ namespace ClassicSuitRestoration
             {
                 Plugin.Logger.LogWarning("Trying to edit unlockables when StartOfRound is not initialized yet, this shouldn't happen normally");
                 UnlockablesList[] unlockablesLists = Resources.FindObjectsOfTypeAll<UnlockablesList>();
-                if (unlockablesLists == null || unlockablesLists.Length > 0)
+                if (unlockablesLists == null || unlockablesLists.Length < 1)
                 {
                     Plugin.Logger.LogError("Can't find unlockables list");
                     return;
@@ -30,23 +30,39 @@ namespace ClassicSuitRestoration
                 unlockablesList = unlockablesLists[0];
             }
 
-            UnlockableItem classicSuit = unlockablesList.unlockables.FirstOrDefault(unlockable => unlockable.unlockableName == "Veteran suit");
+            UnlockableItem classicSuit = unlockablesList.unlockables.FirstOrDefault(unlockable => unlockable.suitMaterial?.name == "OldSuit");
             if (classicSuit == null)
             {
                 classicSuit = new()
                 {
-                    unlockableName = "Veteran suit",
-                    suitMaterial = Object.Instantiate(unlockablesList.unlockables[0].suitMaterial)
+                    suitMaterial = Object.Instantiate(unlockablesList.unlockables[0].suitMaterial),
+                    headCostumeObject = null
                 };
                 classicSuit.suitMaterial.name = "OldSuit";
                 try
                 {
                     AssetBundle classicSuitBundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "classicsuitrestoration"));
-                    Texture scavengerPlayerModel = classicSuitBundle.LoadAsset<Texture>("ScavengerPlayerModel");
-                    classicSuit.suitMaterial.SetTexture("_MainTex", scavengerPlayerModel);
-                    classicSuit.suitMaterial.SetTexture("_BaseColorMap", scavengerPlayerModel);
-                    classicSuit.suitMaterial.SetColor("_BaseColor", Color.white);
-                    classicSuit.suitMaterial.SetTexture("_NormalMap", classicSuitBundle.LoadAsset<Texture>("ScavengerPlayerModel 1"));
+                    Texture suitTexture = null;
+                    if (Plugin.configOfficial.Value)
+                    {
+                        suitTexture = classicSuitBundle.LoadAsset<Texture>("ScavengerPlayerModel");
+                        if (Plugin.configBirthday.Value || System.DateTime.Today.DayOfYear == new System.DateTime(2023, 10, 23).DayOfYear)
+                        {
+                            classicSuit.unlockableName = "Birthday suit";
+                            classicSuit.headCostumeObject = classicSuitBundle.LoadAsset<GameObject>("PartyHatContainer");
+                        }
+                        else
+                            classicSuit.unlockableName = "Brown suit";
+                    }
+                    else
+                    {
+                        suitTexture = classicSuitBundle.LoadAsset<Texture>("OldSuitMockUp");
+                        classicSuit.suitMaterial.SetColor("_BaseColor", Color.white);
+                        classicSuit.suitMaterial.SetTexture("_NormalMap", classicSuitBundle.LoadAsset<Texture>("ScavengerPlayerModel 1"));
+                        classicSuit.unlockableName = "Classic suit";
+                    }
+                    classicSuit.suitMaterial.SetTexture("_MainTex", suitTexture);
+                    classicSuit.suitMaterial.SetTexture("_BaseColorMap", suitTexture);
                     classicSuitBundle.Unload(false);
                 }
                 catch (System.Exception e)
